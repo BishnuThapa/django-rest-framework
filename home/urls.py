20f6564bbbb380f9a2df15027c18eb454e275a52
Bishnu <thapabishnu20@gmail.com>
2023-03-18 19:49:10 +0545

    put & patch functionality for student added

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('student/', views.post_student, name='post-student'),
+    path('update-student/<int:id>/', views.udpate_student, name='update-student'),
     path('delete-student/<int:id>/', views.delete_student, name='delete-student'),
 ]
--- home/views.py
+++ home/views.py
@@ -24,7 +24,23 @@ def post_student(request):
     return Response({'status': 200, 'payload': serializer.data, 'message': 'Saved Successfully!'})
 
 
-@api_view(['DELETE'])
+# put for partial update & add partial=True at serializer object as line 33
+@api_view(['PUT'])
+def udpate_student(request, id):
+    try:
+        student_obj = Student.objects.get(id=id)
+        serializer = StudentSerializer(
+            student_obj, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response({'status': 403, 'errors': serializer.errors, 'message': 'Update failed!'})
+        serializer.save()
+        return Response({'status': 200, 'payload': serializer.data, 'message': 'Updated successfully !'})
+
+    except Exception as e:
+        return Response({'status': 403, 'message': 'invalid id'})
+
+
+@ api_view(['DELETE'])
 def delete_student(request, id):
     try:
         student_obj = Student.objects.get(id=id)

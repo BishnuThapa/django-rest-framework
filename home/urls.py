00124100a85b208e2c1635dfb5370c416d2ac059
Bishnu <thapabishnu20@gmail.com>
2023-03-18 10:26:46 +0545

    deltee api for student implemented

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('student/', views.post_student, name='post-student')
+    path('student/', views.post_student, name='post-student'),
+    path('delete-student/<int:id>/', views.delete_student, name='delete-student'),
 ]
--- home/views.py
+++ home/views.py
@@ -22,3 +22,13 @@ def post_student(request):
         return Response({'status': 403, 'errors': serializer.errors, 'message': 'Something went wrong'})
     serializer.save()
     return Response({'status': 200, 'payload': serializer.data, 'message': 'Saved Successfully!'})
+
+
+@api_view(['DELETE'])
+def delete_student(request, id):
+    try:
+        student_obj = Student.objects.get(id=id)
+        student_obj.delete()
+        return Response({'status': 200, 'message': 'Deleted Successfully!'})
+    except Exception as e:
+        return Response({'status': 403, 'message': 'invalid id'})

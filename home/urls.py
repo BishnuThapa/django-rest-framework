69ce32fec51721062179914c1c2dc12bda8eac89
Bishnu <thapabishnu20@gmail.com>
2023-04-24 20:54:51 +0545

    get all students using APIView views DRF

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('get-book/', views.get_book, name='get-book'),
-    path('student/', views.post_student, name='post-student'),
-    path('update-student/<int:id>/', views.udpate_student, name='update-student'),
-    path('delete-student/<int:id>/', views.delete_student, name='delete-student'),
+    # path('', views.home, name='home'),
+    # path('get-book/', views.get_book, name='get-book'),
+    # path('student/', views.post_student, name='post-student'),
+    # path('update-student/<int:id>/', views.udpate_student, name='update-student'),
+    # path('delete-student/<int:id>/', views.delete_student, name='delete-student'),
+    path('student/', views.StudentAPI.as_view())
 ]
--- home/views.py
+++ home/views.py
@@ -5,57 +5,79 @@ from .models import *
 from .serializers import *
 # Create your views here.
 
+
+from rest_framework.views import APIView
 # MIXINS=> ARE A WAY OF REUSING VIEW LOGIC IN DJANGO REST FRAMEWORK
 
 
-@api_view(['GET'])
-def get_book(request):
-    book_obj = Book.objects.all()
-    serializer = BookSerializer(book_obj, many=True)
+class StudentAPI(APIView):
+    def get(self, request):
+        student_obj = Student.objects.all()
+        serializer = StudentSerializer(student_obj, many=True)
 
-    return Response({'status': 200, 'payload': serializer.data})
+        return Response({'status': 200, 'payload': serializer.data})
 
-# @api_view(['POST', 'GET'])
+    def post(self, request):
+        pass
 
+    def put(self, request):
+        pass
 
-@api_view(['GET'])
-def home(request):
-    student_obj = Student.objects.all()
-    serializer = StudentSerializer(student_obj, many=True)
+    def patch(self, request):
+        pass
 
-    return Response({'status': 200, 'payload': serializer.data})
-
-
-@api_view(['POST'])
-def post_student(request):
-    serializer = StudentSerializer(data=request.data)
-    if not serializer.is_valid():
-        return Response({'status': 403, 'errors': serializer.errors, 'message': 'Something went wrong'})
-    serializer.save()
-    return Response({'status': 200, 'payload': serializer.data, 'message': 'Saved Successfully!'})
+    def delete(self, request):
+        pass
 
 
-# PATCH for partial update & add partial=True at serializer object as line 33
-@api_view(['PUT'])
-def udpate_student(request, id):
-    try:
-        student_obj = Student.objects.get(id=id)
-        serializer = StudentSerializer(
-            student_obj, data=request.data, partial=True)
-        if not serializer.is_valid():
-            return Response({'status': 403, 'errors': serializer.errors, 'message': 'Update failed!'})
-        serializer.save()
-        return Response({'status': 200, 'payload': serializer.data, 'message': 'Updated successfully !'})
+# @api_view(['GET'])
+# def get_book(request):
+#     book_obj = Book.objects.all()
+#     serializer = BookSerializer(book_obj, many=True)
 
-    except Exception as e:
-        return Response({'status': 403, 'message': 'invalid id'})
+#     return Response({'status': 200, 'payload': serializer.data})
+
+# # @api_view(['POST', 'GET'])
 
 
-@ api_view(['DELETE'])
-def delete_student(request, id):
-    try:
-        student_obj = Student.objects.get(id=id)
-        student_obj.delete()
-        return Response({'status': 200, 'message': 'Deleted Successfully!'})
-    except Exception as e:
-        return Response({'status': 403, 'message': 'invalid id'})
+# @api_view(['GET'])
+# def home(request):
+#     student_obj = Student.objects.all()
+#     serializer = StudentSerializer(student_obj, many=True)
+
+#     return Response({'status': 200, 'payload': serializer.data})
+
+
+# @api_view(['POST'])
+# def post_student(request):
+#     serializer = StudentSerializer(data=request.data)
+#     if not serializer.is_valid():
+#         return Response({'status': 403, 'errors': serializer.errors, 'message': 'Something went wrong'})
+#     serializer.save()
+#     return Response({'status': 200, 'payload': serializer.data, 'message': 'Saved Successfully!'})
+
+
+# # PATCH for partial update & add partial=True at serializer object as line 33
+# @api_view(['PUT'])
+# def udpate_student(request, id):
+#     try:
+#         student_obj = Student.objects.get(id=id)
+#         serializer = StudentSerializer(
+#             student_obj, data=request.data, partial=True)
+#         if not serializer.is_valid():
+#             return Response({'status': 403, 'errors': serializer.errors, 'message': 'Update failed!'})
+#         serializer.save()
+#         return Response({'status': 200, 'payload': serializer.data, 'message': 'Updated successfully !'})
+
+#     except Exception as e:
+#         return Response({'status': 403, 'message': 'invalid id'})
+
+
+# @ api_view(['DELETE'])
+# def delete_student(request, id):
+#     try:
+#         student_obj = Student.objects.get(id=id)
+#         student_obj.delete()
+#         return Response({'status': 200, 'message': 'Deleted Successfully!'})
+#     except Exception as e:
+#         return Response({'status': 403, 'message': 'invalid id'})

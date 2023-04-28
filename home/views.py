5c422ee4bddf2ce352b5bd0dea51545b6f8bcb82
Bishnu <thapabishnu20@gmail.com>
2023-04-28 21:50:19 +0545

    update put functionality added to student api

--- home/views.py
+++ home/views.py
@@ -24,8 +24,17 @@ class StudentAPI(APIView):
         serializer.save()
         return Response({'status': 200, 'payload': serializer.data, 'message': 'Saved Successfully!'})
 
-    def put(self, request):
-        pass
+    def put(self, request, id):
+        try:
+            student_obj = Student.objects.get(id=id)
+            serializer = StudentSerializer(
+                student_obj, data=request.data)
+            if not serializer.is_valid():
+                return Response({'status': 403, 'errors': serializer.errors,     'message': 'Update failed!'})
+            serializer.save()
+            return Response({'status': 200, 'payload': serializer.data,   'message':  'Updated successfully !'})
+        except Exception as e:
+            return Response({'status': 403, 'message': 'invalid id'})
 
     def patch(self, request):
         pass
@@ -65,17 +74,17 @@ class StudentAPI(APIView):
             # # PATCH for partial update & add partial=True at serializer object as line 33
             # @api_view(['PUT'])
             # def udpate_student(request, id):
-            #     try:
-            #         student_obj = Student.objects.get(id=id)
-            #         serializer = StudentSerializer(
-            #             student_obj, data=request.data, partial=True)
-            #         if not serializer.is_valid():
-            #             return Response({'status': 403, 'errors': serializer.errors, 'message': 'Update failed!'})
-            #         serializer.save()
-            #         return Response({'status': 200, 'payload': serializer.data, 'message': 'Updated successfully !'})
+            # try:
+            #     student_obj = Student.objects.get(id=id)
+            #     serializer = StudentSerializer(
+            #         student_obj, data=request.data, partial=True)
+            #     if not serializer.is_valid():
+            #         return Response({'status': 403, 'errors': serializer.errors, 'message': 'Update failed!'})
+            #     serializer.save()
+            #     return Response({'status': 200, 'payload': serializer.data, 'message': 'Updated successfully !'})
 
-            #     except Exception as e:
-            #         return Response({'status': 403, 'message': 'invalid id'})
+            # except Exception as e:
+            #     return Response({'status': 403, 'message': 'invalid id'})
 
             # @ api_view(['DELETE'])
             # def delete_student(request, id):
